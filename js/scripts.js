7a3b5c42f1865d4cc9917506e70c8061db573de5
postolenko <denis.postolenko@gmail.com>
2022-08-07 06:30:48 +0300

    Diplom

---
+++ js/scripts.js
@@ -0,0 +1,129 @@
+var w = window,
+d = document,
+e = d.documentElement,
+g = d.getElementsByTagName('body')[0],
+bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
+
+
+$(window).load(function() {
+
+
+
+});
+
+$(window).resize(function() {
+
+
+
+});
+
+$(document).scroll(function() {
+
+
+
+});
+
+$(document).ready(function() {
+
+    if( $(".slider").length > 0 ) {
+        $(".slider").not(".slick-initialized").slick({
+            dots: true,
+            arrows: true,
+            // autoplay: true,
+            autoplaySpeed: 4000,
+            speed: 1200,
+            variableWidth: true,
+            slidesToShow: 3,
+            slidesToScroll: 2,
+            centerMode: true,
+            appendDots: $(".slider_dots"),
+            appendArrows: $(".slider_arrows"),
+            // fade: true,
+            // responsive: [
+            //     {
+            //       breakpoint: 900,
+            //       settings: {
+            //         slidesToShow: 2,
+            //         slidesToScroll: 2
+            //       }
+            //     },
+            //     {
+            //       breakpoint: 540,
+            //       settings: {
+            //         slidesToShow: 1,
+            //         slidesToScroll: 1
+            //       }
+            //     }
+            //   ]
+        });
+    }
+
+    // -------------
+
+    $(document).on("click", ".checkout_list [data-option-val]",  function(e) {
+      e.preventDefault();
+      parent = $(this).closest(".custom_select");
+      val = $(this).attr("data-option-val");
+      parent.find(".p_val").text(val);
+      parent.find("input[type='hidden']").val(val);
+      parent.removeClass("active");
+      parent.find("[data-option-val]").removeClass("active");
+      $(this).addClass("active");
+    });
+
+    $(document).on("click", ".custom_select .p_val", function(e) {
+      e.preventDefault();
+      parent = $(this).closest(".custom_select");
+      parent.toggleClass("active");
+    });
+
+    $(document).mouseup(function(e) {
+      hide_element = $(".custom_select");
+      if (!hide_element.is(e.target)
+          && hide_element.has(e.target).length === 0) {
+          hide_element.removeClass("active");
+        }
+    });
+
+    $(this).keydown(function(eventObject){
+      if (eventObject.which == 27) {
+        $(".custom_select").removeClass("active");
+      }
+    });
+
+    // -----------
+
+    new AirDatepicker('#data', {
+      dateFormat: 'dd.MM.yy'
+    });
+
+    new AirDatepicker('#time',{
+      timepicker: true,
+      onlyTimepicker: true
+    });
+
+    // ------------
+
+    $(".dr").each(function() {
+      drContent = $(this).find(".dr_content");
+      if($(this).hasClass("active")) {      
+        drContent.slideDown(300);
+      } else {
+        drContent.slideUp(300);
+      }
+    });
+
+    $(".dr_title").on("click", function(e) {
+      e.preventDefault();
+      parent = $(this).closest(".dr");
+      sl = parent.find(".dr_content");
+      if(sl.is(":hidden")) {
+        parent.addClass("active");
+        sl.slideDown(300);
+      } else {
+        parent.removeClass("active");
+        sl.slideUp(300);
+      }
+    });
+
+});
